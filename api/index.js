8218608bc5f05e20e5588676d73722ae0182bb63
tarunaditya <tarun.aditya@knowdisdata.com>
2024-05-20 17:39:28 +0530

    Changes for package.json, vercel config and index.js

--- api/index.js
+++ api/index.js
@@ -1,13 +1,13 @@
 const express = require("express");
 const app = express();
 require("dotenv").config();
-const dbConnection = require("./DB/dbConnection");
+const dbConnection = require("../DB/dbConnection");
 const cors = require("cors");
-const userRoutes = require("./routes/user.route");
+const userRoutes = require("../routes/user.route");
 const cookieParser = require("cookie-parser");
-const verifyToken = require("./middleware/verifyToken");
-const threadRoutes = require("./routes/thread.route");
-const postRoutes = require("./routes/post.route");
+const verifyToken = require("../middleware/verifyToken");
+const threadRoutes = require("../routes/thread.route");
+const postRoutes = require("../routes/post.route");
 
 dbConnection();
 
@@ -20,9 +20,7 @@ app.use(cors(corsOptions));
 app.use(express.json());
 app.use(cookieParser());
 
-app.get("/", (req,res) => {
-  res.send("Hello")
-})
+
 app.use("/api/auth", userRoutes);
 app.use("/api/thread", verifyToken, threadRoutes);
 app.use("/api/post", verifyToken, postRoutes);
@@ -30,3 +28,5 @@ app.use("/api/post", verifyToken, postRoutes);
 app.listen(process.env.PORT, () => {
   console.log(`App listening on port ${process.env.PORT}`);
 });
+
+module.exports = app;
