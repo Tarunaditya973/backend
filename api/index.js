51a7a58a9af6b86937736e8233c62468ecea8a18
tarunaditya <tarun.aditya@knowdisdata.com>
2024-05-20 17:54:21 +0530

    Test with db connection

--- api/index.js
+++ api/index.js
@@ -11,19 +11,19 @@ const postRoutes = require("../routes/post.route");
 
 dbConnection();
 
-const corsOptions = {
-  origin: "https://frontend-beta-jade-56.vercel.app",
-  credentials: true,
-};
+// const corsOptions = {
+//   origin: "https://frontend-beta-jade-56.vercel.app",
+//   credentials: true,
+// };
 
-app.use(cors(corsOptions));
-app.use(express.json());
-app.use(cookieParser());
+// app.use(cors(corsOptions));
+// app.use(express.json());
+// app.use(cookieParser());
 
 
-app.use("/api/auth", userRoutes);
-app.use("/api/thread", verifyToken, threadRoutes);
-app.use("/api/post", verifyToken, postRoutes);
+// app.use("/api/auth", userRoutes);
+// app.use("/api/thread", verifyToken, threadRoutes);
+// app.use("/api/post", verifyToken, postRoutes);
 
 // app.listen(process.env.PORT, () => {
 //   console.log(`App listening on port ${process.env.PORT}`);
